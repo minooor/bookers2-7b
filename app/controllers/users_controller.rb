bd3674dbbfb3217e4894836ba5fa1000db22d2d4
EC2 Default User <ec2-user@ip-172-31-13-209.ec2.internal>
2022-02-27 08:25:26 +0000

    [Add] DM機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @books = @user.books
     @book = Book.new
+
+    @current_entry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
+    unless @user.id == current_user.id
+      @current_entry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
 
   def index
@@ -25,7 +43,7 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       redirect_to user_path(@user.id), notice: "You have updated user successfully."
     else
-      
+
       render "edit"
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,9 @@ Rails.application.routes.draw do
 
   end
 
+  resources :messages, only: [:create]
+  resources :rooms, only: [:create, :index, :show, :destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
